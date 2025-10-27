9d3326c679e920629e25141dec0c72820cfcfb42
Operatore∇ <54327323+SpectreCoded@users.noreply.github.com>
2025-10-27 17:51:48 +0100

    Fix config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,10 @@ import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable } from "hardhat/config";
+import hardhatEthers from "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
+  plugins: [hardhatToolboxViemPlugin, hardhatEthers],
   solidity: {
     profiles: {
       default: {
