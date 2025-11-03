cc2daf43b3521f321ffd59e65ef1921992ea5c34
Operatore∇ <54327323+SpectreCoded@users.noreply.github.com>
2025-11-03 16:29:18 +0100

    Setup cliente ed ordine

---
+++ contracts/ordine.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract ordine{
+    struct Ordine{
+        string id;
+        string indirizzoConsegna;
+        string nomeCliente;
+        string cognomeCliente;
+        string citofono;
+    }
+}
